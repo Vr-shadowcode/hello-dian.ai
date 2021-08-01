0f3fb8ccac37841b9674bbf143678950e0fa7d27
Vr-shadow <61784112+Vr-shadowcode@users.noreply.github.com>
2021-08-01 15:51:05 +0800

    Update functional.py

--- lab1/nn/functional.py
+++ lab1/nn/functional.py
@@ -9,15 +9,18 @@ class Sigmoid(Module):
         # TODO Implement forward propogation
         # of sigmoid function.
         ...
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
         # End of todo
 
     def backward(self, dy):
 
         # TODO Implement backward propogation
         # of sigmoid function.
-
         ...
-
+        dx = dy * self.out * (1-self.out)
+        return dx
         # End of todo
 
 
@@ -27,18 +30,19 @@ class Tanh(Module):
 
         # TODO Implement forward propogation
         # of tanh function.
-
         ...
-
+        out = 2 / (1+np.exp(-2*x)) - 1
+        self.out = out
+        return out
         # End of todo
 
     def backward(self, dy):
 
         # TODO Implement backward propogation
         # of tanh function.
-
         ...
-
+        dx = (1-self.out*self.out) * dy
+        return dx
         # End of todo
 
 
@@ -48,18 +52,23 @@ class ReLU(Module):
 
         # TODO Implement forward propogation
         # of ReLU function.
-
         ...
-
+        self.mask = (x <= 0)
+        out = x.copy
+        # 将x<0的初始化为0
+        out[self.mask] = 0
+        return out
         # End of todo
 
     def backward(self, dy):
 
         # TODO Implement backward propogation
         # of ReLU function.
-
         ...
-
+        # 小于0的位置，反向传播的误差为0
+        dy[self.mask] = 0
+        dx = dy
+        return dx
         # End of todo
 
 
@@ -69,18 +78,25 @@ class Softmax(Module):
 
         # TODO Implement forward propogation
         # of ReLU function.
-
         ...
+        if x.ndim == 2:
+            x = x - np.max(x,axis=1)
+            self.out = np.exp(x) / np.sum(np.exp(x),axis=1)
+            return self.out
 
+        x = x - np.max(x)
+        self.out = np.exp(x) / np.sum(np.exp(x))
+
+        return self.out
         # End of todo
 
     def backward(self, dy):
 
         # TODO Implement backward propogation
         # of ReLU function.
-
         ...
-
+        dx = (np.diag(self.out)-np.outer(self.out,self.out)) * dy
+        return dx
         # End of todo
 
 
@@ -114,18 +130,28 @@ class SoftmaxLoss(Loss):
     def __call__(self, probs, targets):
 
         # TODO Calculate softmax loss.
-
         ...
-
+        self.probs = Softmax(probs)
+        self.targets = targets
+        self.loss = CrossEntropyLoss(self.probs,self.targets)
+        # self.dp = None
         # End of todo
 
     def backward(self):
 
         # TODO Implement backward propogation
         # of softmax loss function.
-
         ...
+        batch_size = self.targets.shape[0]
+        if self.targets.size == self.probs.size:
+            dx = (self.probs - self.targets) / batch_size
 
+        else:
+            dx = self.probs.copy()
+            dx[np.arange(batch_size),self.targets] -= 1
+            dx = dx / batch_size
+
+        return dx
         # End of todo
 
 
@@ -134,8 +160,17 @@ class CrossEntropyLoss(Loss):
     def __call__(self, probs, targets):
 
         # TODO Calculate cross-entropy loss.
+        self.probs = Softmax(probs)
+        if probs.ndim == 1:
+            self.probs = probs.reshape(1,probs.size)
+            self.targets = targets.reshape(1,targets.size)
 
-        ...
+        if probs.size == targets.size:
+            self.targets = targets.argmax(axis=1)
+
+        batch_size = self.probs.shape[0]
+
+        self.loss = -np.sum(np.log(self.probs[np.arange(batch_size),targets] + 1e-7)) / batch_size
 
         # End of todo
 
@@ -143,7 +178,16 @@ class CrossEntropyLoss(Loss):
 
         # TODO Implement backward propogation
         # of cross-entropy loss function.
-
         ...
+        # dx = - (self.targets / self.probs)
+        batch_size = self.targets.shape[0]
 
+        if self.targets.size == self.probs.size:
+            dx = (self.probs - self.targets) / batch_size
+
+        else:
+            dx = self.probs.copy()
+            dx[np.arange(batch_size,self.targets)] -= 1
+            dx = dx / batch_size
+        return dx
         # End of todo
